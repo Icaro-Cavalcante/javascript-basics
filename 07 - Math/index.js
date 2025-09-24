e92b4c429d26699828cad037613c5321a857bf9e
Icaro-Cavalcante <icarodev06@gmail.com>
2025-09-24 16:11:03 -0300

    07 - Math
    - Estudos sobre o objeto embutido math.

---
+++ 07 - Math/index.js
@@ -0,0 +1,50 @@
+// Math - Objeto embutido que fornece uma variedade de propriedades e métodos
+
+// O número pi
+pi = Math.PI
+console.log(pi) // output: 3.141592653589793
+
+// Para arredondar números
+let z = Math.round(5.78995) 
+console.log(z) // output: 6
+
+// Arredondar
+let x = Math.floor(2.9) // Arredondar para baixo
+let y = Math.ceil(3.1) // Arredondar para cima
+console.log(x, y) // output: 2 4
+
+// Elimina as casas decimais
+z = Math.trunc(4.545454) 
+console.log(z) // output: 4
+
+// Potenciação (2 elevado a 3)
+z = Math.pow(2, 3) 
+console.log(z) // output: 8
+
+// Raiz quadrada
+z = Math.sqrt(9) 
+console.log(z) // Output: 3
+
+z = Math.log10(100) // Logaritmo
+console.log(z) // Output: 2
+
+// Seno, cosseno, tangente
+z = Math.sin(45) 
+console.log(z) // output: 0.8509035245341184
+y = Math.cos(60)
+console.log(x) // output: 2
+x = Math.tan(30)
+console.log (x) // output: -6.405331196646276
+
+// Módulo
+z = Math.abs(-48)
+console.log(z) // output: 48
+
+// Máximo e mínimo
+x = 10
+y = 5
+z = 54
+w = Math.max(x, y, z)
+console.log(w) // output: 54
+w = Math.min(x, y, z)
+console.log(w) // output: 5
