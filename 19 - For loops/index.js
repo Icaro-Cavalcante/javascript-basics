4950beda806e92ef3fd8c85f9aa610a9c5a189b3
Icaro-Cavalcante <icarodev06@gmail.com>
2025-10-09 23:27:20 -0300

    19 - For loops

---
+++ 19 - For loops/index.js
@@ -0,0 +1,10 @@
+// For loop - Executa um código um número limitado de vezes
+
+for (let i = 0; i < 20 + 1; i+=1) {
+    if (i % 2 == 0) {
+        console.log(`${i} é par.`)
+    }
+    else {
+        console.log(`${i} é ímpar.`)
+    }
+}
